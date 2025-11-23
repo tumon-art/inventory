c483cbc0a6b816091079137a707938dc218b4441
tumon <mofakhar0@gmail.com>
2025-11-23 15:56:17 +0600

    .

--- server/products.ts
+++ server/products.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/db/drizzle";
 import { products, Product } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function getProducts() {
   try {
@@ -14,11 +15,29 @@ export async function getProducts() {
 
 }
 
-export async function createUser(user: Omit<Product, "id" | "createdAt" | "updatedAt">) {
+export async function createProduct(user: Omit<Product, "id" | "createdAt" | "updatedAt">) {
   try {
     await db.insert(products).values(user);
   } catch (error) {
     console.error(error);
     return { error: "Failed to create user" };
+  }
+}
+
+export async function updateProduct(user: Omit<Product, "createdAt" | "updatedAt">) {
+  try {
+    await db.update(products).set(user).where(eq(products.id, user.id));
+  } catch (error) {
+    console.error(error);
+    return { error: "Failed to update user" };
+  }
+}
+
+export async function deleteProduct(id: string) {
+  try {
+    await db.delete(products).where(eq(products.id, id));
+  } catch (error) {
+    console.error(error);
+    return { error: "Failed to delete user" };
   }
 }
